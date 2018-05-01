b682045629b5b33689ec8202343c7e90a096a38a
Liesware <liesware@hotmail.com>
2018-04-30 19:43:15 -0500

    Add files via upload

--- coherence02/coherence.c
+++ coherence02/coherence.c
@@ -160,7 +160,7 @@ void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
   if (buf->base) {
       free(buf->base);
   } 
- 
+  uv_close((uv_handle_t*) client, NULL); 
 }
 
 void on_new_connection(uv_stream_t *server, int status) {
@@ -172,7 +172,7 @@ void on_new_connection(uv_stream_t *server, int status) {
   uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
   uv_tcp_init(loop, client);
   if (uv_accept(server, (uv_stream_t*) client) == 0) {
-      uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);
+      uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);      
   } 
   else {
     uv_close((uv_handle_t*) client, NULL);
