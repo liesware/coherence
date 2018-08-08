f86afe1cdd65cbdf8496a774392e44299431144a
Liesware <liesware@hotmail.com>
2018-08-07 19:26:40 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkdir  coherence_git
+cd coherence_git
+git clone https://github.com/liesware/coherence
+cd coherence/coherence02/lib/
+
+git clone https://github.com/P-H-C/phc-winner-argon2
+mv phc-winner-argon2/ argon2
+cd argon2
+make
+
+cd ..
+git clone https://github.com/weidai11/cryptopp
+cd cryptopp
+make libcryptopp.a libcryptopp.so cryptest.exe
+
+cd ..
+git clone https://github.com/tbuktu/libntru.git
+cd libntru
+make
+make static-lib
+
+cd ..
+git clone https://github.com/libuv/libuv.git
+cd libuv
+sh autogen.sh
+./configure
+make
+
+cd ..
+git clone https://github.com/Tencent/rapidjson.git
+
+cd ..
+make -f Makefile.static
