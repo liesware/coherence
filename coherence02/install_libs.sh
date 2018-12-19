016c9e65c139ff08cc4e7790d05f1e545e6b03b6
Liesware <liesware@hotmail.com>
2018-12-19 13:15:27 -0600

    Add files via upload

---
+++ coherence02/install_libs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp lib/libuv/.libs/libuv.so.1 /lib64/
+cp lib/cryptopp/libcryptopp.so.7 /lib64/
+cp lib/libntru/libntru.so /lib64/
+cp lib/liboqs/.libs/liboqs.so.0 /lib64/
+cp lib/argon2/libargon2.so.1 /lib64/
+
