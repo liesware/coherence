0bc87a7d8736dabf36953b2ec2cb2948720f41bc
Liesware <liesware@hotmail.com>
2019-02-05 10:59:02 -0600

    Add files via upload

--- coherence02/coherence.c
+++ coherence02/coherence.c
@@ -17,6 +17,7 @@ struct sockaddr_in addr;
 
 ofstream log_file;
 
+
 ////////////////////////////////////////////////////////////////////////
 void banner(){
   printf("Welcome to Cryptoserver\n");
@@ -53,32 +54,51 @@ void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
   memset (buf->base,'\0',suggested_size);
 }
 
+
 void on_close(uv_handle_t* handle) {
   free(handle);
 }
 
-void echo_write(uv_write_t *req, int status) {
+
+void on_write(uv_write_t *req, int status) {
   if(status) {
     fprintf(stderr, "Write error %s\n", uv_strerror(status));
   }
   free(req);
 }
 
-void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
-  if (nread < 0) {
-      if (nread != UV_EOF) {
-          fprintf(stderr, "Read error %s\n", uv_err_name(nread));
-          try{
-            uv_close((uv_handle_t*) client, on_close);
-          }
-          catch(int * e){
-             cout << "An exception occurred. " << e << '\n';
-          }
-          return;
-      }
+
+void on_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+  char global_buf[4194304];
+
+ if (nread < 0) {
+    #ifdef DEBUG
+    fprintf(stderr, "%ld bytes read\n", nread);
+    #endif
+
+    free(buf->base);
+    uv_close((uv_handle_t*) client, on_close);
+    return;
   }
-  else if (nread > 0) {
+
+  if (nread >= 0) {
+    if((strlen(global_buf)+nread)<sizeof(global_buf))
+      strncat (global_buf, buf->base, nread);
+
+    #ifdef DEBUG
+    fprintf(stderr, "%ld bytes read\n", nread);
+    printf("%s\n", global_buf);
+    #endif
+
     stru_info_log log_info;
+    string answer;
+
+  //if(global_buf[strlen(global_buf)-1]=='\n' || global_buf[strlen(global_buf)-1]=='}' || nread == UV_EOF ){
+    #ifdef DEBUG
+    fprintf(stderr, "Procesing client request\n");
+    fprintf(stderr, "Total read:    %d\n", total_read);
+    printf("Global_buf %s\n", global_buf);
+    #endif
 
     uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
 
@@ -91,69 +111,77 @@ void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
 
     log_info.timestamp=(int)time(NULL);
     log_info.ip=ip;
-    log_info.req=buf->base;
+    log_info.req=global_buf;
 
-#ifdef DEBUG
+    #ifdef DEBUG
     printf("\nClient connect: %s\n", log_info.ip.c_str());
     printf("Timestamp: %d\n",log_info.timestamp);
     printf("Recived: %s\n",log_info.req.c_str());
-#endif
+    #endif
 
     clock_t t;
     t = clock();
 
     string str_json;
     str_json.clear();
-    str_json = buf->base;
-    string answer;
+    str_json = global_buf;
 
     if(ok_buff(buf)!=0){
       answer.clear();
-	    answer="{\"error\":\"Bad Buffer, plase see https://github.com/liesware/coherence  and report bugs\"}";
-#ifdef DEBUG
-	  printf("\nSended: %s\n",answer.c_str());
-#endif
+      answer="{\"error\":\"Bad Buffer, plase see https://github.com/liesware/coherence  and report bugs\"}";
+      #ifdef DEBUG
+      printf("\nSended: %s\n",answer.c_str());
+      #endif
       log_info.answ=answer;
 
-	    char *paramsp=strdup( answer.c_str());
-	    uv_buf_t wrbuf= uv_buf_init(paramsp, answer.length());
-	    uv_write(req, client, &wrbuf, 1, echo_write);
-	    free(paramsp);
+      char *paramsp=strdup( answer.c_str());
+      uv_buf_t wrbuf= uv_buf_init(paramsp, answer.length());
+      uv_write(req, client, &wrbuf, 1, on_write);
+      free(paramsp);
 
-	    t = clock()-t;
+      t = clock()-t;
       log_info.exec_time=(float)t/CLOCKS_PER_SEC;
+      log_info.total_read=log_info.req.length();
+      log_info.total_write=log_info.answ.length();
       string log_js="{}";
       log_info.req="{\"error\":\"Bad json string format request\"}";
       parse_log(log_info, log_js);
       cout<<log_js<<endl;
 
-	}
-  else{
+    }
+    else{
 
-	  PARSING(str_json , answer);
-#ifdef DEBUG
-	  printf("\nSended: %s\n",answer.c_str());
-#endif
+      PARSING(str_json , answer);
+      #ifdef DEBUG
+      printf("\nSended: %s\n",answer.c_str());
+      #endif
       log_info.answ=answer;
 
-	  char *paramsp=strdup( answer.c_str());
-	  uv_buf_t wrbuf= uv_buf_init(paramsp, answer.length());
-	  uv_write(req, client, &wrbuf, 1, echo_write);
-	  free(paramsp);
+      char *paramsp=strdup( answer.c_str());
+      uv_buf_t wrbuf= uv_buf_init(paramsp, answer.length());
+      uv_write(req, client, &wrbuf, 1, on_write);
+      free(paramsp);
 
-	  t = clock()-t;
-    log_info.exec_time=(float)t/CLOCKS_PER_SEC;
-    string log_js="{}";
-    parse_log(log_info, log_js);
-    cout<<log_js<<endl;
+      t = clock()-t;
+      log_info.exec_time=(float)t/CLOCKS_PER_SEC;
+      log_info.total_read=log_info.req.length();
+      log_info.total_write=log_info.answ.length();
+      string log_js="{}";
+      parse_log(log_info, log_js);
+      cout<<log_js<<endl;
 
-  }
- }
+    }
 
-  free(buf->base);
-  uv_close((uv_handle_t*) client, on_close);
+    free(buf->base);
+    memset (global_buf,'\0',sizeof(global_buf));
+
+    if (nread == UV_EOF) {uv_close((uv_handle_t*) client, on_close);}
 
 }
+
+  return;
+}
+
 
 void on_new_connection(uv_stream_t *server, int status) {
   if(status < 0) {
@@ -164,12 +192,13 @@ void on_new_connection(uv_stream_t *server, int status) {
   uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
   uv_tcp_init(loop, client);
   if (uv_accept(server, (uv_stream_t*) client) == 0) {
-      uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);
+      uv_read_start((uv_stream_t*)client, alloc_buffer, on_read);
   }
   else {
     uv_close((uv_handle_t*) client, on_close);
   }
 }
+
 
 int main(int argc, char *argv[]) {
   if(argc!=3){
