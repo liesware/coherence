6231b3ecfe2101af65f0dc42f0c1cd3841584f57
Liesware <liesware@hotmail.com>
2019-02-22 10:22:52 -0600

    Update cp_libs.sh

--- coherence02/cp_libs.sh
+++ coherence02/cp_libs.sh
@@ -1,8 +1,8 @@
-cp lib/libuv/.libs/libuv.so.1.0.0 /usr/lib/x86_64-linux-gnu/libuv.so.1
-cp lib/cryptopp/libcryptopp.so.8.1.0 /usr/lib/x86_64-linux-gnu/libcryptopp.so.8
-cp lib/libntru/libntru.so /usr/lib/x86_64-linux-gnu/
-cp lib/liboqs/liboqs.so /usr/lib/x86_64-linux-gnu/
-ls -lha /usr/lib/x86_64-linux-gnu/libuv.so.1
-ls -lha /usr/lib/x86_64-linux-gnu/libcryptopp.so.8
-ls -lha /usr/lib/x86_64-linux-gnu/libntru.so
-ls -lha /usr/lib/x86_64-linux-gnu/liboqs.so
+cp lib/libuv/.libs/libuv.so.1.0.0 /lib64//libuv.so.1
+cp lib/cryptopp/libcryptopp.so.8.1.0 /lib64/libcryptopp.so.8
+cp lib/libntru/libntru.so /lib64/
+cp lib/liboqs/liboqs.so /lib64/
+ls -lha /lib64/libuv.so.1
+ls -lha /lib64/libcryptopp.so.8
+ls -lha /lib64/libntru.so
+ls -lha /lib64/liboqs.so
