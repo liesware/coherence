c1901338aa8b4e7b3600dd47af4e642469c564c7
Liesware <liesware@hotmail.com>
2019-02-22 11:39:29 -0600

    Add files via upload

---
+++ coherence02/grpc/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yum install which unzip openssl -y
+git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
+cd grpc
+git submodule update --init
+make
+make install
+cd third_party/protobuf
+make
+make install
+cd ../../../
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export LD_LIBRARY_PATH=/usr/local/lib
+cd ..
+sh cp_libs.sh
+cd grpc/
+make
+sh gen.sh
