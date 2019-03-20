bda2fbb6a419020aa738bcdd87c09afae184adae
Liesware <liesware@hotmail.com>
2019-03-20 17:00:39 -0600

    Add files via upload

--- coherence02/examples/rsa.py
+++ coherence02/examples/rsa.py
@@ -13,7 +13,7 @@ def sending(message):
 	s.close()
 	return data
 
-def rsa(data_js, bits):
+def rsa(data_js, bits, hash_sign):
 	req=json.loads(data_js)
 	req["length"]=bits
 	print "Send gen parameters : \n " + json.dumps(req) +"\n"
@@ -23,6 +23,7 @@ def rsa(data_js, bits):
 	json_s=json_v='{ "version": 1 , "algorithm":"RSA", "type":"string","plaintext": "Hello world!", "hex":0,"privkey": "" ,"operation":"sign"}'
 	req=json.loads(json_s)
 	req["privkey"]=answ["privkey"]
+	req["hash_sign"]=hash_sign
 	print "Send sign : \n " + json.dumps(req) +"\n"
 	data_js_n=sending(json.dumps(req))
 	answ_1=json.loads(data_js_n)
@@ -31,6 +32,7 @@ def rsa(data_js, bits):
 	req=json.loads(json_v)
 	req["pubkey"]=answ["pubkey"]
 	req["sign"]=answ_1["sign"]
+	req["hash_sign"]=hash_sign
 	print "Send verify : \n " + json.dumps(req) +"\n"
 	data_js_n=sending(json.dumps(req))
 	answ_2=json.loads(data_js_n)
@@ -54,4 +56,6 @@ def rsa(data_js, bits):
 
 
 rsa_gen='{ "version": 1 , "algorithm":"RSA", "operation":"gen" , "length": 0 }'
-rsa(rsa_gen,1024)
+hash_sign=["sha3_512","sha3_384","sha3_256","sha3_224","sha_512","sha_384","sha_256","sha_224","sha_1","whirlpool"]
+for i in hash_sign:
+    rsa(rsa_gen,2048,i)
