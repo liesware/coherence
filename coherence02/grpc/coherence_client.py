c1802de8b1192d617a3b207c8542a7a74825340d
Liesware <liesware@hotmail.com>
2019-03-22 21:32:38 -0600

    Add files via upload

--- coherence02/grpc/coherence_client.py
+++ coherence02/grpc/coherence_client.py
@@ -10,23 +10,23 @@ import json
 
 def run():
     message="Hello world !"
-    qesla_gen='{"version":1,"algorithm":"QTESLA", "operation":"gen" ,"parameter":"qteslaiiispeed"}'
-    qtesla_sign='{"version":1,"algorithm":"QTESLA", "operation":"sign", "type":"string" ,"parameter":"qteslaiiispeed"}'
-    qtesla_v='{"version":1,"algorithm":"QTESLA", "operation":"verify", "type":"string" ,"parameter":"qteslaiiispeed"}'
+    ed25519_gen='{ "version": 1 , "algorithm":"ED25519", "operation":"gen"}'
+    ed25519_sign='{ "version": 1 , "algorithm":"ED25519", "type":"string","plaintext": "Hello world!", "hex":0,"privkey": "" ,"operation":"sign"}'
+    ed25519_v='{ "version": 1 , "algorithm":"ED25519", "type":"string","plaintext": "Hello world!", "hex":0,"pubkey": "" ,"sign":"","operation":"verify"}'
     with open('ca.crt', 'rb') as f:
         creds = grpc.ssl_channel_credentials(f.read())
         channel = grpc.secure_channel('localhost:6613', creds)
         stub = coherence_pb2_grpc.coherence_offloadStub(channel)
-        response = stub.coherence_js(coherence_pb2.coherence_req(req=qesla_gen))
+        response = stub.coherence_js(coherence_pb2.coherence_req(req=ed25519_gen))
         print(response.answ)
         answ=json.loads(response.answ)
-        json1=json.loads(qtesla_sign)
+        json1=json.loads(ed25519_sign)
         json1["privkey"]=answ["privkey"]
         json1["plaintext"]=message
         response = stub.coherence_js(coherence_pb2.coherence_req(req=json.dumps(json1)))
         print(response.answ)
         answ1=json.loads(response.answ)
-        json2=json.loads(qtesla_v)
+        json2=json.loads(ed25519_v)
         json2["pubkey"]=answ["pubkey"]
         json2["plaintext"]=message
         json2["sign"]=answ1["sign"]
--- coherence02/grpc/gen.sh
+++ coherence02/grpc/gen.sh
@@ -2,19 +2,25 @@
 
 # Generate valid CA
 openssl genrsa -out ca.key 2048
+#openssl ecparam -name secp521r1 -genkey -out ca.key
 openssl req -new -x509 -days 365 -key ca.key -out ca.crt -subj  "/C=LI/ST=Liesland/L=Lies/O=Test/OU=Test/CN=Root CA"
+openssl x509 -in ca.crt -text -noout
 
 # Generate valid Server Key/Cert
 openssl genrsa -out server.key 2048
+#openssl ecparam -name secp256k1 -genkey -out server.key
 openssl req  -new -key server.key -out server.csr -subj  "/C=LI/ST=Liesland/L=Lies/O=Test/OU=Server/CN=localhost"
 openssl x509 -req  -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+openssl x509 -in server.crt -text -noout
 
 # Remove passphrase from the Server Key
-openssl rsa  -in server.key -out server.key
+#openssl rsa  -in server.key -out server.key
 
 # Generate valid Client Key/Cert
 openssl genrsa -out client.key 2048
+#openssl ecparam -name prime256v1 -genkey -out client.key
 openssl req  -new -key client.key -out client.csr -subj  "/C=LI/ST=Liesland/L=Lies/O=Test/OU=Client/CN=localhost"
-openssl x509  -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
+openssl x509  -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 02 -out client.crt
+openssl x509 -in client.crt -text -noout
 
 python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. coherence.proto
