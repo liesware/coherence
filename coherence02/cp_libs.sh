7924331b6701eee88dbfcb19b46fd076c07fba9f
Liesware <liesware@hotmail.com>
2019-09-05 13:13:36 -0500

    Update cp_libs.sh
    Migracion a debian

--- coherence02/cp_libs.sh
+++ coherence02/cp_libs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp lib/libuv/.libs/libuv.so.1.0.0 /lib64/libuv.so.1
-cp lib/cryptopp/libcryptopp.so.8.2.0 /lib64/libcryptopp.so.8
-cp lib/libntru/libntru.so /lib64/
-cp lib/liboqs/.libs/liboqs.so.0.0.0 /lib64/liboqs.so.0
-cp lib/argon2/libargon2.so.1 /lib64/
-ls -lha /lib64/libuv.so.1
-ls -lha /lib64/libcryptopp.so.8
-ls -lha /lib64/libntru.so
-ls -lha /lib64/liboqs.so.0
-ls -lha /lib64/libargon2.so.1 
+cp lib/libuv/.libs/libuv.so.1.0.0 /lib/x86_64-linux-gnu/libuv.so.1
+cp lib/cryptopp/libcryptopp.so.8.2.0 /lib/x86_64-linux-gnu/libcryptopp.so.8
+cp lib/libntru/libntru.so /lib/x86_64-linux-gnu/
+cp lib/liboqs/.libs/liboqs.so.0.0.0 /lib/x86_64-linux-gnu/liboqs.so.0
+cp lib/argon2/libargon2.so.1 /lib/x86_64-linux-gnu/
+ls -lha /lib/x86_64-linux-gnu/libuv.so.1
+ls -lha /lib/x86_64-linux-gnu/libcryptopp.so.8
+ls -lha /lib/x86_64-linux-gnu/libntru.so
+ls -lha /lib/x86_64-linux-gnu/liboqs.so.0
+ls -lha /lib/x86_64-linux-gnu/libargon2.so.1
