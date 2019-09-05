e34a4b36258cf0677b0a30d5898fa661bd75e49b
Liesware <liesware@hotmail.com>
2019-09-05 15:51:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,10 +40,17 @@ cd pistache
 git submodule update --init
 mkdir -p {build,prefix}
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-make
+cmake -G "Unix Makefiles" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DPISTACHE_BUILD_EXAMPLES=false \
+        -DPISTACHE_BUILD_TESTS=false \
+        -DPISTACHE_BUILD_DOCS=false \
+        -DPISTACHE_USE_SSL=false \
+        -DCMAKE_INSTALL_PREFIX=$PWD/../prefix \
+        ../
+make -j
 make install
 
-cd ../../
+cd ../../../
 mkdir bin
 make
