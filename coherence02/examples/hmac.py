64de00551dc057bc6f36683f8e6dd3c30127504a
Liesware <liesware@hotmail.com>
2019-09-05 17:22:22 -0500

    Update hmac.py

--- coherence02/examples/hmac.py
+++ coherence02/examples/hmac.py
@@ -3,9 +3,9 @@ import json
 import os,binascii
 
 def sending(message):
-	url = 'http://127.0.0.1:6613/'
-	response=requests.post(url, data=message)
-	return response.content
+    url = 'http://127.0.0.1:6613/'
+    response=requests.post(url, data=message)
+    return response.content
 
 algorithms=["sha3_512", "sha3_384", "sha3_256", "sha3_224", "sha_512", "sha_384", "sha_256", "sha_224", "sha_1" ,"whirlpool"]
 
