b43f405188e8375be828d0da5ca290eb073bfe69
Liesware <liesware@hotmail.com>
2021-09-19 19:17:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,7 +71,6 @@ make
 
 cd grpc
 git clone --recurse-submodules -b v1.32.0 https://github.com/grpc/grpc
-# git clone --recurse-submodules -b v1.38.0 https://github.com/grpc/grpc
 cd grpc
 mkdir -p cmake/build
 cd cmake/build
