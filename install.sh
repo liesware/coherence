48cde4d2a2e6413940b0f4a90078cc0f99edfa03
Liesware <liesware@hotmail.com>
2024-07-24 21:24:29 -0600

    Update install.sh
    new libs

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # docker rmi liesware/coherence:rd
 # docker commit coherence_rd liesware/coherence:rd
 # docker login
-# docker push liesware/coherence:rd
+# docker push liesware/coherence:rds
 
 apt-get update
 DEBIAN_FRONTEND="noninteractive" apt-get install -y autoconf automake gcc g++ make libtool git wget unzip xsltproc libssl-dev bzip2 valgrind doxygen graphviz python3 python3-pip cmake libcurl4-openssl-dev cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz git wget libargon2-dev pkg-config meson astyle python3-yaml
@@ -23,7 +23,7 @@ make
 cd ..
 mkdir cryptopp
 cd cryptopp
-wget https://www.cryptopp.com/cryptopp890.zip
+wget https://github.com/weidai11/cryptopp/releases/download/CRYPTOPP_8_9_0/cryptopp890.zip
 unzip cryptopp890.zip
 make libcryptopp.a libcryptopp.so
 
@@ -31,10 +31,10 @@ cd ..
 git clone https://github.com/Tencent/rapidjson.git
 
 # git clone -b main https://github.com/open-quantum-safe/liboqs.git
-wget https://github.com/open-quantum-safe/liboqs/archive/refs/tags/0.9.1.zip
-unzip 0.9.1.zip
-mv liboqs-0.9.1 liboqs
-mv 0.9.1.zip liboqs
+wget https://github.com/open-quantum-safe/liboqs/archive/refs/tags/0.10.1.zip
+unzip 0.10.1.zip
+mv liboqs-0.10.1 liboqs
+mv liboqs-0.10.1.zip liboqs
 cd liboqs
 mkdir build && cd build
 cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
@@ -42,7 +42,7 @@ ninja
 
 cd ../../
 git clone https://github.com/open-quantum-safe/liboqs-cpp
-sed -i '40 i std::string LIBOQS_CPP_VERSION="0.9.1";' liboqs-cpp/include/common.hpp
+sed -i '40 i std::string LIBOQS_CPP_VERSION="0.10.1";' liboqs-cpp/include/common.hpp
 
 git clone https://github.com/pistacheio/pistache.git
 cd pistache
@@ -53,9 +53,9 @@ cd ../../
 mkdir bin
 make
 
-cp lib/pistache/build/src/libpistache.so.0.2.7 /lib/x86_64-linux-gnu/libpistache.so.0
+cp lib/pistache/build/src/libpistache.so.0.3.1 /lib/x86_64-linux-gnu/libpistache.so.0
 cp lib/cryptopp/libcryptopp.so.8.9.0 /lib/x86_64-linux-gnu/libcryptopp.so.8
-cp lib/liboqs/build/lib/liboqs.so.0.9.1 /lib/x86_64-linux-gnu/liboqs.so.4
+cp lib/liboqs/build/lib/liboqs.so.0.10.1 /lib/x86_64-linux-gnu/liboqs.so.4
 ls -lha /lib/x86_64-linux-gnu/libpistache.so.0
 ls -lha /lib/x86_64-linux-gnu/libcryptopp.so.8
 ls -lha /lib/x86_64-linux-gnu/liboqs.so.4
